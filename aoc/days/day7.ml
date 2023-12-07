86d402c3d6f5f43063bb81a9334e332e3c259e0a
ZetkoOfficial <info@zetkoofficial.com>
2023-12-07 17:20:37 +0100

    zoptimizirano še sortiranje

--- aoc/days/day7.ml
+++ aoc/days/day7.ml
@@ -26,12 +26,10 @@ module Resitev : Solution.GenericSolution = struct
     List.length @@ List.filter (fun (_,num) -> n = num) (CMap.bindings occ)
   ;;
 
+  let card_list = ['A',1;'K',2;'Q',3;'J',4;'T',5;'9',6;'8',7;'7',8;'6',9;'5',10;'4',11;'3',12;'2',13];;
   let card_str c is_part2 = 
-    let order = 
-      if is_part2 then ['A',1;'K',2;'Q',3;'J',14;'T',5;'9',6;'8',7;'7',8;'6',9;'5',10;'4',11;'3',12;'2',13]
-                  else ['A',1;'K',2;'Q',3;'J',4;'T',5;'9',6;'8',7;'7',8;'6',9;'5',10;'4',11;'3',12;'2',13] in
-
-    List.assoc c order
+    if c = 'J' && is_part2 then 14
+    else List.assoc c card_list
   ;;
 
   let hand_to_val hand = 
@@ -56,27 +54,28 @@ module Resitev : Solution.GenericSolution = struct
     hand_to_val (List.map (fun c -> if c = 'J' then cmn else c) hand)
   ;;
 
-  let compare_hands hand1 hand2 is_part2 =
-    let c1 = 
-      if is_part2 then compare (best_hand_val hand1) (best_hand_val hand2)
-      else compare (hand_to_val hand1) (hand_to_val hand2)
-    in
+  let compare_hands (hand1,val1) (hand2,val2) is_part2 =
+    let c1 = compare val1 val2 in
 
     if c1 <> 0 then c1
     else List.compare (fun a b -> compare (card_str a is_part2) (card_str b is_part2)) hand1 hand2
   ;;
 
-  let solve1 hands = 
-    let sorted = List.rev @@ List.sort (fun (hand1,_) (hand2,_) -> compare_hands hand1 hand2 false) hands in
+  let solve with_values is_part2 = 
+    let sorted = List.rev @@ List.sort (fun (h1,_) (h2,_) -> compare_hands h1 h2 is_part2) with_values in
     let winnings = List.mapi (fun i (_,bid) -> (i+1)*bid) sorted in
 
     [ string_of_int @@ List.fold_left (+) 0 winnings ]
   ;;
-  let solve2 hands = 
-    let sorted = List.rev @@ List.sort (fun (hand1,_) (hand2,_) -> compare_hands hand1 hand2 true) hands in
-    let winnings = List.mapi (fun i (_,bid) -> (i+1)*bid) sorted in
 
-    [ string_of_int @@ List.fold_left (+) 0 winnings ]
+  let solve1 hands = 
+    let with_values = List.map (fun (hand,bid) -> ((hand, hand_to_val hand), bid)) hands in
+    solve with_values false
+  ;;
+
+  let solve2 hands =
+    let with_values = List.map (fun (hand,bid) -> ((hand, best_hand_val hand), bid)) hands in
+    solve with_values true
   ;;
 
 end
