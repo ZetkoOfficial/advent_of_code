53f12d7c9e92e4caa8b24fb7706b1a3d02995e7e
ZetkoOfficial <info@zetkoofficial.com>
2023-12-15 23:26:40 +0100

    malce pospešen dan14

--- aoc/days/day14.ml
+++ aoc/days/day14.ml
@@ -7,18 +7,40 @@ module Resitev : Solution.GenericSolution = struct
   end
   module IIMap = Map.Make(II)
 
-  type p_in = (char list) list
+  type grid_elem = Rock | Wall | Empty
+  type p_in = (grid_elem list) list
 
-  let parse in_channel = 
-    in_channel |> Solution.read_lines |> List.map (fun line -> List.of_seq @@ String.to_seq line)
+  let compare elem1 elem2 = 
+    match elem1, elem2 with
+    | Rock, Rock -> 0  | Empty, Empty -> 0
+    | Rock, Empty -> 1 | Empty, Rock -> -1
+    | _ -> 0
   ;;
 
-  let split_by_wall slice = 
+  let from_char char = 
+    match char with
+    | 'O' -> Rock | '#' -> Wall | '.' -> Empty
+    | _ -> failwith "invalid symbol"
+  ;;
+
+  let parse in_channel = 
+    in_channel |> Solution.read_lines |> List.map (fun line -> List.map (from_char) (List.of_seq @@ String.to_seq line))
+  ;;
+
+  (* let split_by_wall slice = 
     let acc,rem = List.fold_left (fun (acc,rem) char ->
-      if char = '#' then (List.rev rem)::acc, []
+      if char = Wall then (List.rev rem)::acc, []
       else acc, char::rem
     ) ([],[]) slice in
     List.rev ((List.rev rem)::acc)
+  ;; *)
+
+  let split_by_wall slice = 
+    let acc,rem = List.fold_right (fun char (acc,rem) ->
+      if char = Wall then rem::acc, []
+      else acc, char::rem
+    ) slice ([],[]) in
+    rem::acc
   ;;
 
   let sort_split rev slices = 
@@ -29,7 +51,7 @@ module Resitev : Solution.GenericSolution = struct
   ;;
 
   let reconstruct slices = 
-    List.tl @@ List.fold_left (fun slice acc -> slice @ '#'::acc) [] slices;;
+    List.tl @@ List.fold_left (fun slice acc -> slice @ Wall::acc) [] slices;;
   ;;
 
   let move_dir rev input = List.map (fun col -> col |> split_by_wall |> sort_split rev |> reconstruct) input;;
@@ -47,11 +69,11 @@ module Resitev : Solution.GenericSolution = struct
     input |> move Up |> move Left |> move Down |> move Right
   ;; 
 
-  let calc_weight input =
-    let input = Solution.transpose input in 
+  let calc_weight do_transpose input =
+    let input = if do_transpose then Solution.transpose input else input in 
     List.fold_left (fun acc col ->
       let acc',_ = List.fold_left (fun (acc,i) char -> 
-        if char = 'O' then acc+i,i+1
+        if char = Rock then acc+i,i+1
         else acc,i+1  
       ) (acc, 1) (List.rev col) in
       acc'
@@ -59,14 +81,14 @@ module Resitev : Solution.GenericSolution = struct
   ;;
 
   let rec find_cycle i map input = 
-    let hash = calc_weight input, calc_weight @@ Solution.transpose input in
+    let hash = calc_weight true input, calc_weight false input in
     match IIMap.find_opt hash map with
     | None -> find_cycle (i+1) (IIMap.add hash i map) (move_cycle input)
     | Some i' -> i', (i-i'), map
   ;;
 
   let solve1 input = 
-    [ move Up input |> calc_weight |> string_of_int ]
+    [ move Up input |> calc_weight true |> string_of_int ]
   ;;
 
   let solve2 input = 
